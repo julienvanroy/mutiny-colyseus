9444b70de5d278930ca21fed27fee8e1d062f1e7
MorganeSauneuf <morgane.sauneuf@gmail.com>
2022-05-16 15:57:44 +0200

    feat: add color to player

--- src/rooms/schema/Player.ts
+++ src/rooms/schema/Player.ts
@@ -4,6 +4,7 @@ export class Player extends Schema {
 
   @type ("string") id:string;
   @type("string") name:string = "";
+  @type("string") color:string = "";
   @type("number") points:number = 0;
 
 }
