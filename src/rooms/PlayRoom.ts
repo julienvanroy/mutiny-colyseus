612b97af02ab59aea6b1b2480b1c725eac60b6a4
Julien Vanroy <julien.vanroy@akqa.com>
2022-05-17 11:41:50 +0200

    feat: add points for player

--- src/rooms/PlayRoom.ts
+++ src/rooms/PlayRoom.ts
@@ -47,6 +47,9 @@ export class PlayRoom extends Room<State> {
     });
 
     this.onMessage("addPoint", (client, message) => {
+      const player = this.state.players.get(message.playerId);
+      player.points += 1;
+
       this.broadcast("addPoint", {
         playerId: message.playerId,
         playerPoints: message.playerPoints
