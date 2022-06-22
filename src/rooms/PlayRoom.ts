031c5e2105f0e29c3c79acacb0e502fdaf93842e
Thi Van An Truong <antr.2201@gmail.com>
2022-06-22 19:57:43 +0200

    fix: send  player stealer name on updatePlyaerTarget event

--- src/rooms/PlayRoom.ts
+++ src/rooms/PlayRoom.ts
@@ -70,13 +70,17 @@ export class PlayRoom extends Room<State> {
         }
       }
 
-      const target = this.state.players.get(message.playerTarget.id)
+      if (message.targetGotStolen) {
+        const playerStealer = this.state.players.get(message.targetGotStolen);
 
-      if ( target ) {
-        this.broadcast("updatePlayerTarget", {
-          target: target.name,
-        })
+        if (playerStealer) {
+          this.broadcast("updatePlayerTarget", {
+            target: playerStealer.name,
+          })
+        }
       }
+      
+
       this.broadcast("getAllPlayers", this.state.players)
     });
 
